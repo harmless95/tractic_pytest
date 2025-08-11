395733ea065e840270f68fc24609e89dd78d4dff
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-11 21:10:37 +0700

    create main.py

---
+++ my_project/main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def get_hello():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
