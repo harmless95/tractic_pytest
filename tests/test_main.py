ce6ceb5c14c8b4f8230e3338aa2a21c82bf9307d
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-12 13:26:02 +0700

    add test by id pytest parametrize

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,14 +5,17 @@ from my_project.main import app, fake_db
 
 client = TestClient(app=app)
 
+
 @pytest.fixture
 def data_example():
     return {"id": "bar", "title": "Bar", "description": "The bartenders"}
+
 
 @pytest.fixture(autouse=True)
 def fake_db_clear():
     fake_db.clear()
     return fake_db
+
 
 @pytest.fixture
 def add_db():
@@ -24,6 +27,7 @@ def add_db():
     )
     return fake_db
 
+
 @pytest.fixture
 def token_header():
     return {"x-token": "coneofsilence"}
@@ -34,13 +38,22 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
+
 def test_create_item(data_example, token_header):
     response = client.post("/", headers=token_header, json=data_example)
 
     assert response.status_code == 200
     assert response.json() == data_example
 
-def test_by_id(data_example, add_db, token_header):
-    response = client.get("/bar", headers=token_header)
+
+@pytest.mark.parametrize(
+    "url, data",
+    [
+        ("/bar", {"id": "bar", "title": "Bar", "description": "The bartenders"}),
+        ("/foo", {"id": "foo", "title": "Foo", "description": "There goes my hero"}),
+    ],
+)
+def test_by_id(add_db, token_header, url, data):
+    response = client.get(url, headers=token_header)
     assert response.status_code == 200
-    assert response.json() == data_example
+    assert response.json() == data
