a3bcbd39e7d064b829ddd3ff85d981312fe41167
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-12 13:07:24 +0700

    add fixture all clear db, data example and adding to database for searching by id

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,37 @@
+import pytest
 from fastapi.testclient import TestClient
 
-from my_project.main import app
+from my_project.main import app, fake_db
 
 client = TestClient(app=app)
+
+@pytest.fixture
+def data_example():
+    return {"id": "bar", "title": "Bar", "description": "The bartenders"}
+
+@pytest.fixture(autouse=True)
+def fake_db_clear():
+    fake_db.clear()
+    return fake_db
+
+@pytest.fixture
+def add_db(data_example):
+    fake_db[data_example["id"]] = data_example
+    return fake_db
+
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
-def test_create_item():
-    response = client.post("/", headers={"x-token": "coneofsilence"}, json={"id": "foobar", "title": "Foo Bar", "description": "The Foo Barters"})
+def test_create_item(data_example):
+    response = client.post("/", headers={"x-token": "coneofsilence"}, json=data_example)
 
     assert response.status_code == 200
-    assert response.json() == {"id": "foobar", "title": "Foo Bar", "description": "The Foo Barters"}
+    assert response.json() == data_example
 
-def test_by_id():
+def test_by_id(data_example, add_db):
     response = client.get("/bar", headers={"x-token": "coneofsilence"})
     assert response.status_code == 200
-    assert response.json() == {"id": "bar", "title": "Bar", "description": "The bartenders"}
+    assert response.json() == data_example
