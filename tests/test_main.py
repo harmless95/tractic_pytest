f0f349a095ef40063c8039c515d49d02f600f2a3
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-12 13:09:56 +0700

    add fixture token header

--- tests/test_main.py
+++ tests/test_main.py
@@ -19,19 +19,23 @@ def add_db(data_example):
     fake_db[data_example["id"]] = data_example
     return fake_db
 
+@pytest.fixture
+def token_header():
+    return {"x-token": "coneofsilence"}
+
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
-def test_create_item(data_example):
-    response = client.post("/", headers={"x-token": "coneofsilence"}, json=data_example)
+def test_create_item(data_example, token_header):
+    response = client.post("/", headers=token_header, json=data_example)
 
     assert response.status_code == 200
     assert response.json() == data_example
 
-def test_by_id(data_example, add_db):
-    response = client.get("/bar", headers={"x-token": "coneofsilence"})
+def test_by_id(data_example, add_db, token_header):
+    response = client.get("/bar", headers=token_header)
     assert response.status_code == 200
     assert response.json() == data_example
